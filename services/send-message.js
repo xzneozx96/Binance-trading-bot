a419cc19f84d4a08b988da699cc6485e5b722446
Ross Geller <tuananhngyen218@gmail.com>
2024-06-14 10:04:05 +0700

    remove toFixed() on price

--- services/send-message.js
+++ services/send-message.js
@@ -1,79 +1,70 @@
 const fbAccessToken =
-  "EAANpzAfMX6oBO9yHE0CjM1jqCVnk72wn6xJsVXM7leDHS6tK836cAjUDfi0FGC38WQNoQ4ZAN6qV99kdrMS0CnqxZB1z6Us9hTDnPmXz9DHfXN15dRkjmuqdyuSkWM89wy0sZAid83VD2TVpKv3wfZANafEhvMxCtHZCHQwXEI4OG72FIS0btNGrCoIa0ZBhMZD";
+	'EAANpzAfMX6oBO9yHE0CjM1jqCVnk72wn6xJsVXM7leDHS6tK836cAjUDfi0FGC38WQNoQ4ZAN6qV99kdrMS0CnqxZB1z6Us9hTDnPmXz9DHfXN15dRkjmuqdyuSkWM89wy0sZAid83VD2TVpKv3wfZANafEhvMxCtHZCHQwXEI4OG72FIS0btNGrCoIa0ZBhMZD';
 
 const sendWhaleAlert = async (content) => {
-  const {
-    symbol,
-    avgPrice,
-    percentageChange,
-    kline,
-    orderType,
-    openPrice,
-    closePrice,
-    stopLoss,
-  } = content;
+	const { symbol, avgPrice, percentageChange, kline, orderType, openPrice, closePrice, stopLoss } = content;
 
-  const notiMsg = {
-    recipient: {
-      id: "6351453234935287",
-    },
-    message: {
-      text: `The following cryptocurrency has experienced a significant change:
+	const notiMsg = {
+		recipient: {
+			id: '6351453234935287',
+		},
+		message: {
+			text: `The following cryptocurrency has experienced a significant change:
 
 - Symbol: ${symbol}
 - Average Price: ${avgPrice}
 - Kline Interval: ${kline}
 - Percentage Change: ${percentageChange}%
 - Trade Recommendation:
-	Open ${orderType}: ${openPrice.toFixed(2)},
+	Open ${orderType}: ${openPrice},
 	Margin: x5,
-	Take profit: ${closePrice.toFixed(2)} (30%),
-	Stoploss: ${stopLoss.toFixed(2)} (25%)
+	Take profit: ${closePrice} (30%),
+	Stoploss: ${stopLoss} (25%)
       `,
-    },
-    messaging_type: "MESSAGE_TAG",
-    tag: "ACCOUNT_UPDATE",
-  };
-  console.log("about to send msg", JSON.stringify(notiMsg));
+		},
+		messaging_type: 'MESSAGE_TAG',
+		tag: 'ACCOUNT_UPDATE',
+	};
+	console.log('about to send msg', JSON.stringify(notiMsg));
 
-  const url = `https://graph.facebook.com/v19.0/me/messages?access_token=${fbAccessToken}`;
-  const params = {
-    method: "POST",
-    body: JSON.stringify(notiMsg),
-    headers: { "Content-Type": "application/json" },
-  };
+	const url = `https://graph.facebook.com/v19.0/me/messages?access_token=${fbAccessToken}`;
+	const params = {
+		method: 'POST',
+		body: JSON.stringify(notiMsg),
+		headers: { 'Content-Type': 'application/json' },
+	};
 
-  const req = await fetch(url, params);
-  const body = await req.json();
+	const req = await fetch(url, params);
+	const body = await req.json();
 
-  console.log("send msg success", body);
+	console.log('send msg success', body);
 };
 
 const sendBalanceReport = async (totalBalance) => {
-  const notiMsg = {
-    recipient: {
-      id: "6351453234935287",
-    },
-    message: {
-      text: `Hello Boss, please take a look at your current total balance: ${totalBalance} USD
+	const notiMsg = {
+		recipient: {
+			id: '6351453234935287',
+		},
+		message: {
+			text: `Hello Boss, please take a look at your current total balance: ${totalBalance} USD
       `,
-    },
-    messaging_type: "MESSAGE_TAG",
-    tag: "ACCOUNT_UPDATE",
-  };
-  console.log("about to send msg", JSON.stringify(notiMsg));
+		},
+		messaging_type: 'MESSAGE_TAG',
+		tag: 'ACCOUNT_UPDATE',
+	};
+	console.log('about to send msg', JSON.stringify(notiMsg));
 
-  const url = `https://graph.facebook.com/v19.0/me/messages?access_token=${fbAccessToken}`;
-  const params = {
-    method: "POST",
-    body: JSON.stringify(notiMsg),
-    headers: { "Content-Type": "application/json" },
-  };
+	const url = `https://graph.facebook.com/v19.0/me/messages?access_token=${fbAccessToken}`;
+	const params = {
+		method: 'POST',
+		body: JSON.stringify(notiMsg),
+		headers: { 'Content-Type': 'application/json' },
+	};
 
-  const req = await fetch(url, params);
-  const body = await req.json();
+	const req = await fetch(url, params);
+	const body = await req.json();
 
-  console.log("send msg success", body);
+	console.log('send msg success', body);
 };
 
 module.exports = { sendWhaleAlert, sendBalanceReport };
